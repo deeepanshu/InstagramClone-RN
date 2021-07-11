567284d46fff459f057ed688161fd7e41ee15c1e
Deepanshu Jain <deepanshu@md-rpm.com>
2021-07-11 14:52:46 +0530

    Stories View Complete

--- src/screens/StoryScreen/index.js
+++ src/screens/StoryScreen/index.js
@@ -3,13 +3,18 @@ import {
   ActivityIndicator,
   Dimensions,
   ImageBackground,
-  SafeAreaView, Text,
+  SafeAreaView,
+  Text,
+  TextInput,
   TouchableWithoutFeedback,
-} from "react-native";
+  View,
+} from 'react-native';
 import {useNavigation, useRoute} from '@react-navigation/native';
 import {STORIES_DATA} from '../../data/stories';
 import styles from './styles';
-import ProfilePicture from "../../components/ProfilePicture";
+import ProfilePicture from '../../components/ProfilePicture';
+import Feather from 'react-native-vector-icons/Feather';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const StoryScreen = () => {
   const route = useRoute();
@@ -71,10 +76,23 @@ const StoryScreen = () => {
       <TouchableWithoutFeedback onPress={event => handleStoryClick(event)}>
         <ImageBackground
           style={styles.image}
-          source={{uri: activeStory.imageUri}}
-        >
-          <ProfilePicture uri={userStories.user.imageUri} />
-          <Text>{userStories.user.name}</Text>
+          source={{uri: activeStory.imageUri}}>
+          <View style={styles.userInfo}>
+            <ProfilePicture uri={userStories.user.imageUri} size={40} />
+            <Text style={styles.userName}>{userStories.user.name}</Text>
+            <Text style={styles.postedTime}>{activeStory.postedTime}</Text>
+          </View>
+          <View style={styles.bottomContainer}>
+            <View style={styles.cameraButton}>
+              <Feather name="camera" size={30} color="#fff" />
+            </View>
+            <View style={styles.textContainer}>
+              <TextInput placeholder={'Send Message'} editable placeholderTextColor={'#fff'} style={styles.textInput} />
+            </View>
+            <View style={styles.messageButton}>
+              <Ionicons name="paper-plane-outline" size={30} color="#fff" />
+            </View>
+          </View>
         </ImageBackground>
       </TouchableWithoutFeedback>
     </SafeAreaView>
--- src/screens/StoryScreen/styles.js
+++ src/screens/StoryScreen/styles.js
@@ -7,7 +7,55 @@ const styles = StyleSheet.create({
   image: {
     flex: 1,
     resizeMode: 'cover',
+    justifyContent: 'space-between',
+  },
+  userInfo: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 10,
+  },
+  userName: {
+    color: '#fff',
+    fontWeight: '700',
+    fontSize: 18,
+  },
+  bottomContainer: {
+    flexDirection: 'row',
+    marginBottom: 20,
+    marginHorizontal: 10,
+  },
+  cameraButton: {
+    width: 50,
+    alignItems: 'center',
     justifyContent: 'center',
+    borderWidth: 1,
+    borderColor: '#fff',
+    borderRadius: 50,
+  },
+  messageButton: {
+    width: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textContainer: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: '#fff',
+    borderRadius: 50,
+    height: 50,
+    marginHorizontal: 10,
+    paddingHorizontal: 10,
+  },
+  textInput: {
+    height: '100%',
+    color: '#fff',
+    fontSize: 18,
+  },
+  postedTime: {
+    color: '#808080',
+    marginLeft: 10,
+    fontSize: 18,
+    fontWeight: '700',
   },
 });
 
